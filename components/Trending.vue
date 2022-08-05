d4d9bca179c75e52b080ccb92d3a450ef164e0ff
Cancey Dejean <canceyd@gmail.com>
2022-08-04 22:36:33 -0500

    Updated

--- components/Trending.vue
+++ components/Trending.vue
@@ -1,5 +1,5 @@
 <template>
-  <section class="relative py-[200px] overflow-hidden overflow-x-scroll">
+  <section class="relative py-[200px] overflow-hidden">
     <img
       class="absolute w-[200%] top-[50%] right-0 transform -translate-y-[50%]"
       src="~/assets/images/trend-bg.svg"
@@ -7,16 +7,17 @@
     />
 
     <div class="container px-0">
-      <div class="flex w-[200%]">
-        <TrendingCard
+      <TrendingSlider />
+      <div class="flex">
+        <!-- <TrendingCard
           tag="Postermywall music records"
           number="01"
           imageName="t-cover-1"
           altText="Trending Cover"
           title="Electronic"
           desc="Ken Adams Mixtape"
-        />
-        <TrendingCard
+        /> -->
+        <!-- <TrendingCard
           tag="Postermywall music records"
           number="02"
           imageName="t-cover-2"
@@ -39,7 +40,7 @@
           altText="Trending Cover"
           title="Tangerine dream"
           desc="Rubycon"
-        />
+        /> -->
       </div>
     </div>
   </section>
@@ -47,9 +48,11 @@
 
 <script>
 import TrendingCard from "./microComponents/TrendingCard.vue";
+import TrendingSlider from "./microComponents/TrendingSlider.vue";
+
 export default {
   name: "Trending",
-  components: { TrendingCard },
+  components: { TrendingCard, TrendingSlider },
 };
 </script>
 
